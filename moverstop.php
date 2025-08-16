c82ae2ad22e06ac04daa5b14a793c02319ec787d
vizolini <ralph.halvorsen@gmx.com>
2025-08-17 00:52:25 +0100

    Create moverstop.php

---
+++ moverstop.php
@@ -0,0 +1,31 @@
+<?php
+/* moverstop.php
+ * Adds a "Stop Mover" button to the Unraid Main tab
+ */
+
+$docroot = $docroot ?? $_SERVER['DOCUMENT_ROOT'] ?: '/usr/local/emhttp';
+require_once "$docroot/webGui/include/Helpers.php";
+
+// Hook into Unraid header to inject JS
+add_hook('Main', 'addStopMoverButton');
+
+function addStopMoverButton() {
+?>
+<script type="text/javascript">
+$(function() {
+    let moverBtn = $('button:contains("Move Now")');
+
+    if (moverBtn.length && $('#stopMoverBtn').length === 0) {
+        $('<button id="stopMoverBtn" class="btn">Stop Mover</button>')
+            .insertAfter(moverBtn)
+            .on('click', function() {
+                $.post('/update.php', { cmd: 'mover stop' }, function(data) {
+                    alert("Mover stop command sent!");
+                });
+            });
+    }
+});
+</script>
+<?php
+}
+?>
