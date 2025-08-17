da2ab4757ce8d36ed45a498a4af16543efc5ad9d
vizolini <ralph.halvorsen@gmx.com>
2025-08-17 01:23:59 +0100

    Update moverstop.php

--- moverstop.php
+++ moverstop.php
@@ -6,21 +6,18 @@
 $docroot = $docroot ?? $_SERVER['DOCUMENT_ROOT'] ?: '/usr/local/emhttp';
 require_once "$docroot/webGui/include/Helpers.php";
 
-// Hook into Unraid Main tab
 add_hook('Main', 'addStopMoverButton');
 
 function addStopMoverButton() {
 ?>
 <script type="text/javascript">
 $(function() {
-    // Look for the "Move" button
     let moverBtn = $('button:contains("Move")').first();
-
     if (moverBtn.length && $('#stopMoverBtn').length === 0) {
         $('<button id="stopMoverBtn" class="btn">Stop Mover</button>')
             .insertAfter(moverBtn)
             .on('click', function() {
-                $.post('/update.php', { cmd: 'mover stop' }, function(data) {
+                $.post('/update.php', { cmd: 'mover stop' }, function() {
                     alert("Mover stop command sent!");
                 });
             });
